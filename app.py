93403f325a77cb4c105337de00e02b6566ec47f7
coneastdev <coneastdev@proton.me>
2026-01-12 19:06:06 +0000

    add functionality

--- app.py
+++ app.py
@@ -1,10 +1,76 @@
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QFileDialog, QComboBox
-from PySide6.QtGui import QIntValidator  # Import QIntValidator
 import os
+import platform
+from datetime import datetime
 
-def execute(input: str, output: str, outputCompression: int, outputFormat: str):
-    return ""
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QFileDialog, QComboBox, QDialog
+from PySide6.QtGui import QIntValidator
+
+sys = platform.system()
+
+def execute(inputPath: str, outputCompression: str, outputFormat: str, outputName: str, output: str):
+    # notify user their is no input and terminate function
+    if inputPath == "":
+        return ["blank input"]
     
+    # if same as input will replace output format with the inputs format
+    if outputFormat == "Same As Input":
+        inputFormat = []
+        splitInput: list = list(inputPath)
+        index: int = -1
+        
+        # reverse for loop to find last occurrence of "."
+        while True:
+            inputFormat.insert(0, splitInput[index])
+            if splitInput[index] == ".":
+                break
+            else:
+                index -= 1
+        outputFormat: str = "".join(inputFormat)
+        
+    # set output to desktop if blank
+    if output == "":
+        desktop = ""
+        if sys == "Linux":
+            desktop = f"/home/{os.getlogin()}/Desktop"
+        elif sys == "Windows":
+            desktop = "C:\\Users\\" + os.environ.get("USERNAME") + "\\Desktop"
+        
+        output = desktop
+    
+    # set file name to current date if empty
+    if outputName == "":
+        outputName = str(datetime.now()).replace(" ", "_")
+
+    if sys == "Linux":
+        output: str = output + "/" + outputName + outputFormat
+        os.chdir("/")
+    elif sys == "Windows":
+        output: str = "\\" + output + "\\" + outputName + outputFormat
+        os.chdir("C:\\")
+
+    # stops spaces in path from creating an error
+    inputPath = inputPath.replace(" ", "\\ ")
+
+    # try so it outputs the error in the gui and not terminal
+    try:
+        os.system(f"ffmpeg -i {inputPath} {"-crf " + str(outputCompression) + " " if not (outputCompression == "") else ""}{output}")
+        
+        # remove file from output path so when you click the link it opens the folder instead of the video
+        splitOutput: list = list(output)
+  
+        while True:
+            splitInput.pop(-1)
+            if splitInput[-1] == "/":
+                break
+            
+        cleanedOutput: str = "".join(splitInput)
+        
+        return ["completed", cleanedOutput]
+    
+    except Exception as e:
+        return [e]
+    
+# the main qt window
 def app():
     qt_app = QApplication([])
     window = QWidget()
@@ -34,7 +100,10 @@ def app():
     layout.addWidget(outputFormatLabel)
     
     outputFormatComboBox = QComboBox()
-    outputFormatComboBox.addItems(["Same As Input", ".mp4", ".avi", ".mov", ".wmv", ".mkv", ".webm", ".mpeg", ".flv", ".mts", ".gif", ".mp3", ".wav", ".ogg"])
+    outputFormatComboBox.addItems(["Same As Input", ".mp4", ".avi", ".mov", 
+                                   ".wmv", ".mkv", ".webm", ".mpeg", ".flv", 
+                                   ".mts", ".gif", ".mp3", ".wav", ".ogg"])
+    
     layout.addWidget(outputFormatComboBox)
     
     # output compression
@@ -42,17 +111,22 @@ def app():
     layout.addWidget(compressionLabel)
     
     compressionInput = QLineEdit()
-    compressionInput.setValidator(QIntValidator(0, 51))  # Set validator for range 0-51
-    compressionInput.setText("0")  # Set default value to 0
+    compressionInput.setValidator(QIntValidator(0, 51))
     layout.addWidget(compressionInput)
     
-    # remove leading zeros
+    # stop leading zeros
     def removeLeadingZeros():
         text = compressionInput.text()
         if text.startswith("0") and len(text) > 1:
             compressionInput.setText(text.lstrip("0"))
-
-    compressionInput.textChanged.connect(removeLeadingZeros)  # Connect signal to function
+    compressionInput.textChanged.connect(removeLeadingZeros)
+    
+    # file name
+    fileNameLabel = QLabel("Output File Name")
+    layout.addWidget(fileNameLabel)
+    
+    fileNameInput = QLineEdit()
+    layout.addWidget(fileNameInput)
     
     # output folder
     outputPathLabel = QLabel("File Path For Output Folder")
@@ -75,12 +149,46 @@ def app():
     layout.addWidget(generateButton)
     
     def generateButtonClicked():
-        output: str = outputPathInput.text()
-        input: str = inputPathInput.text()
-        outputCompression: int = int(compressionInput.text())
+        # collect the inputs for execute function
+        inputPath: str = inputPathInput.text()
+        outputCompression: str = compressionInput.text()
         outputFormat: str = outputFormatComboBox.currentText()
+        outputName: str = fileNameInput.text()
+        output: str = outputPathInput.text()
+                
+        # process events to keep the dialog responsive
+        QApplication.processEvents()
         
-        execute(output, input, outputCompression, outputFormat)
+        notice = execute(inputPath, outputCompression, outputFormat, 
+                         outputName, output)
+        
+        dialog = QDialog(window)
+        dialog.setWindowTitle("py-ffmpeg-gui notification")
+        dLayout = QVBoxLayout()
+        dialog.setMinimumSize(300,150)
+            
+        dLabel = QLabel()
+        dLayout.addWidget(dLabel)
+        dialog.setLayout(dLayout)
+        
+        if notice[0] == "completed":
+            dLabel.setText(("Completed, exported to <a href='{}'>" + 
+                            notice[1] + "</a>").format(notice[1]))
+            
+            dLabel.setOpenExternalLinks(True)
+        elif notice[0] == "blank input":
+            dLabel.setText("ERROR: No input video selected.")
+        else:
+            dLabel.setText(str(notice[0]))
+            
+        dBtn = QPushButton()
+        dBtn.setText("Close")
+        dLayout.addWidget(dBtn)
+        dBtn.clicked.connect(dialog.close)
+        
+        dialog.setLayout(dLayout)
+        dialog.show()
+        QApplication.processEvents()
     generateButton.clicked.connect(generateButtonClicked)
     
     window.setLayout(layout)
