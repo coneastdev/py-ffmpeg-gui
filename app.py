82e2404d6afb9c58e0c33ddabcf41f0e2504d283
coneastdev <coneastdev@proton.me>
2026-01-23 13:38:15 +0000

    make code more abstracted and add doc strings

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os
-import platform
+import pathlib
 from datetime import datetime
 
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QFileDialog, QComboBox, QDialog
 from PySide6.QtGui import QIntValidator
-
-sys = platform.system()
-
-def execute(inputPath: str, outputCompression: str, outputFormat: str, outputName: str, output: str):
-    # notify user their is no input and terminate function
-    if inputPath == "":
-        return ["blank input"]
     
-    # if same as input will replace output format with the inputs format
+# replace output format with the inputs format
+def replaceOutputFormatWithInput(inputPath: str, outputFormat: str):
+    """
+    set output video format to the sa,e as the input videos format
+    
+    Returns:
+        str: Output format i.e. .mp4
+    """
     if outputFormat == "Same As Input":
         inputFormat = []
         splitInput: list = list(inputPath)
@@ -27,50 +27,119 @@ def execute(inputPath: str, outputCompression: str, outputFormat: str, outputNam
                 index -= 1
         outputFormat: str = "".join(inputFormat)
         
-    # set output to desktop if blank
-    if output == "":
-        desktop = ""
-        if sys == "Linux":
-            desktop = f"/home/{os.getlogin()}/Desktop"
-        elif sys == "Windows":
-            desktop = "C:\\Users\\" + os.environ.get("USERNAME") + "\\Desktop"
-        
-        output = desktop
+    return outputFormat
+
+# set output to local users desktop
+def setOutputToDesktop():
+    """
+    Set the output path to the user's desktop directory.
+
+    Returns:
+        str: The path to the desktop directory.
+    """
+    return f"{str(pathlib.Path.home())}/Desktop"
     
-    # set file name to current date if empty
-    if outputName == "":
-        outputName = str(datetime.now()).replace(" ", "_")
+# set file name to current date
+def genOutputName():
+    """
+    set the output name to the date and time
+    
+    Returns:
+        str: Date time in format of year-month-day_hour:minutes.seconds.
+    """
+    outputName = str(datetime.now()).replace(" ", "_")
+    return outputName
+    
+# stops spaces in path from creating an error
+def replaceSpacesWithBackSlash(path: str):
+    """
+    replace spaces with backslash's
+        
+    Returns:
+        str: Fixed path.
+    """
+    return path.replace(" ", "\\ ")
 
-    if sys == "Linux":
-        output: str = output + "/" + outputName + outputFormat
-        os.chdir("/")
-    elif sys == "Windows":
-        output: str = "\\" + output + "\\" + outputName + outputFormat
-        os.chdir("C:\\")
-
-    # stops spaces in path from creating an error
-    inputPath = inputPath.replace(" ", "\\ ")
-
+# function to run the command
+def execute(inputPath: str, outputCompression: str, outputFormat: str, outputName: str, output: str):
+    """
+    execute ffmpeg command with given arguments
+    
+    Returns:
+        list: Completion status and dialog messages in the format of [completion status, dialog message]
+    """
     # try so it outputs the error in the gui and not terminal
     try:
-        os.system(f"ffmpeg -i {inputPath} {"-crf " + str(outputCompression) + " " if not (outputCompression == "") else ""}{output}")
-        
-        # remove file from output path so when you click the link it opens the folder instead of the video
-        splitOutput: list = list(output)
-  
-        while True:
-            splitOutput.pop(-1)
-            if splitOutput[-1] == "/":
-                break
-            
-        cleanedOutput: str = "".join(splitOutput)
-        
-        return ["completed", cleanedOutput]
+        os.system(f"ffmpeg -i {inputPath} {"-crf " + str(outputCompression) + " " if not (outputCompression == "") else ""}{output + "/" + outputName + outputFormat}")
+        return ["completed", output]
     
     except Exception as e:
         return [e]
     
+# gathers, cleans and executes commands    
+def generateButtonClicked(window: QWidget, inputPathInput: QLineEdit, compressionInput: QLineEdit, outputFormatComboBox: QComboBox, fileNameInput: QLineEdit, outputPathInput: QLineEdit):
+    # collect the inputs for execute function
+    inputPath: str = inputPathInput.text()
+    outputCompression: str = compressionInput.text()
+    outputFormat: str = outputFormatComboBox.currentText()
+    outputName: str = fileNameInput.text()
+    output: str = outputPathInput.text()
+            
+    # process events to keep the dialog responsive
+    QApplication.processEvents()
+    
+    # if blank warn else clean and execute
+    if inputPath == "":
+        notice = ["blank input"]
+    else:
+        # make output format same as input format
+        if outputFormat == "Same As Input":
+            outputFormat: str = replaceOutputFormatWithInput(inputPath, outputFormat)
+            
+        # set output to desktop if empty
+        if output == "":
+            output: str = setOutputToDesktop()
+            
+        # set output name to date if empty
+        if outputName == "":
+            outputName: str = genOutputName()
+            
+        # stop space in inputs and outputs from breaking 
+        inputPath: str = replaceSpacesWithBackSlash(inputPath)
+        output: str = replaceSpacesWithBackSlash(output)
+        
+        notice: list = execute(inputPath, outputCompression, outputFormat, outputName, output)
+    
+    # create a popup dialog box
+    dialog = QDialog(window)
+    dialog.setWindowTitle("py-ffmpeg-gui notification")
+    dLayout = QVBoxLayout()
+    dialog.setMinimumSize(300,150)
+        
+    dLabel = QLabel()
+    dLayout.addWidget(dLabel)
+    dialog.setLayout(dLayout)
+    
+    if notice[0] == "completed":
+        dLabel.setText((f"Completed, exported to <a href='{notice[1]}'>{notice[1]}</a>"))
+        
+        dLabel.setOpenExternalLinks(True)
+    elif notice[0] == "blank input":
+        dLabel.setText("ERROR: No input video selected.")
+    else:
+        dLabel.setText(str(notice[0]))
+            
+    dBtn = QPushButton()
+    dBtn.setText("Close")
+    dLayout.addWidget(dBtn)
+    dBtn.clicked.connect(dialog.close)
+        
+    dialog.setLayout(dLayout)
+    dialog.show()
+    QApplication.processEvents()
+    
 # the main qt window
+
 def app():
     qt_app = QApplication([])
     window = QWidget()
@@ -144,52 +213,13 @@ def app():
             outputPathInput.setText(directory)
     outputPathButton.clicked.connect(onOutputPathClicked)
     
+    def generateButtonClick():
+        generateButtonClicked(window, inputPathInput, compressionInput, outputFormatComboBox, fileNameInput, outputPathInput)
+    
     # generate button
     generateButton = QPushButton("generate")
     layout.addWidget(generateButton)
-    
-    def generateButtonClicked():
-        # collect the inputs for execute function
-        inputPath: str = inputPathInput.text()
-        outputCompression: str = compressionInput.text()
-        outputFormat: str = outputFormatComboBox.currentText()
-        outputName: str = fileNameInput.text()
-        output: str = outputPathInput.text()
-                
-        # process events to keep the dialog responsive
-        QApplication.processEvents()
-        
-        notice = execute(inputPath, outputCompression, outputFormat, 
-                         outputName, output)
-        
-        dialog = QDialog(window)
-        dialog.setWindowTitle("py-ffmpeg-gui notification")
-        dLayout = QVBoxLayout()
-        dialog.setMinimumSize(300,150)
-            
-        dLabel = QLabel()
-        dLayout.addWidget(dLabel)
-        dialog.setLayout(dLayout)
-        
-        if notice[0] == "completed":
-            dLabel.setText(("Completed, exported to <a href='{}'>" + 
-                            notice[1] + "</a>").format(notice[1]))
-            
-            dLabel.setOpenExternalLinks(True)
-        elif notice[0] == "blank input":
-            dLabel.setText("ERROR: No input video selected.")
-        else:
-            dLabel.setText(str(notice[0]))
-            
-        dBtn = QPushButton()
-        dBtn.setText("Close")
-        dLayout.addWidget(dBtn)
-        dBtn.clicked.connect(dialog.close)
-        
-        dialog.setLayout(dLayout)
-        dialog.show()
-        QApplication.processEvents()
-    generateButton.clicked.connect(generateButtonClicked)
+    generateButton.clicked.connect(generateButtonClick)
     
     window.setLayout(layout)
     window.show()
